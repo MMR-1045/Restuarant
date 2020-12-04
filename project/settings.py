e60837fb4f6742a08ff2f93c0c1687dc75d0cc2b
MMR <32247752+MMR-1045@users.noreply.github.com>
2020-12-04 20:21:08 +0600

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '68rs(e50g7xl__@z4#0f&*8wz_rl5@k@g+l7*3wh()@lduyiuy'
-
+SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
